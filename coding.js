0493541206ffc22472cc63c4c61306811f07826e
Haridi Manzi <manziharidi@gmail.com>
2025-06-27 17:50:50 +0200

    my commit

--- coding.js
+++ coding.js
@@ -1,1 +1,10 @@
-console.log("my name's haridi");
+function changeBackgroundColor(color){
+    let section = document.getElementById('information');
+    section.style.backgroundColor = color;
+}
+
+document.getElementById('information').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
+document.getElementById('home').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
+
+document.getElementById('contact').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+
