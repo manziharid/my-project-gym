c63fc536c3672a8baf522a4ebe733832d07cb025
Haridi Manzi <manziharidi@gmail.com>
2025-07-12 16:00:45 +0200

    my 11'th commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,4 +1,9 @@
-let str = 'abcd';
-for (let a of str) {
-    console.log(a);
+let colors = new Map();
+
+colors.set('red', '#ff0000');
+colors.set('green', '#00ff00');
+colors.set('blue', '#0000ff');
+
+for (let color of colors) {
+    console.log(color);
 }
