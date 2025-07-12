7ba854fe4dce49afaf7fc0cc82c1cb401c960215
Haridi Manzi <manziharidi@gmail.com>
2025-07-12 15:39:17 +0200

    my eightth commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,8 +1,11 @@
-let name = 'machine name';
-let machine = {
-    [name]: 'server',
-    'machine hours': 10000
+let server = {
+    name: 'Server',
+    restart() {
+        console.log("The " + this.name + " is restarting...");
+    },
+    'starting up'() {
+        console.log("The " +  this.name + " is starting up!");
+    }
 };
 
-console.log(machine[name]); // server
-console.log(machine['machine hours']); // 10000
+server['restart']();
