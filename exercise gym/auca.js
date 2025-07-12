e0bf28dcf3ff4c61e7138791ee2079ad81ee0030
Haridi Manzi <manziharidi@gmail.com>
2025-07-12 15:03:14 +0200

    my commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,9 +1,3 @@
-function sum(...args) {
-    let total = 0;
-    for (const a of args) {
-        total += a;
-    }
-    console.log(total);
-}
-
-sum(1, 2, 3, 4);
+const a = ['mazni'];
+const combined = ['haridi', ...a];
+console.log(combined);
