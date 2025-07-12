913c59ef483eaa7161eb44ca40abc55da3a9dc99
Haridi Manzi <manziharidi@gmail.com>
2025-07-12 14:32:45 +0200

    dujh

---
+++ exercise gym/auca.js
@@ -0,0 +1,9 @@
+function put(toy, toyBox = []) {
+    toyBox.push(toy);
+    return toyBox;
+}
+
+console.log(put('Toy Car'));
+// -> ['Toy Car']
+console.log(put('Teddy Bear'));
+// -> ['Teddy Bear'], not ['Toy Car','Teddy Bear']
