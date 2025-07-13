bbe8aeaa845db8849f4cc33d4054ed51d1469fe4
Haridi Manzi <manziharidi@gmail.com>
2025-07-13 13:40:56 +0200

    my 9'th commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,19 +1,2 @@
-function format(literals, ...substitutions) {
-  let result = '';
-
-  for (let i = 0; i < substitutions.length; i++) {
-    result += literals[i];
-    result += substitutions[i];
-  }
-  // add the last literal
-  result += literals[literals.length - 1];
-  return result;
-}
-
-let quantity = 9,
-  priceEach = 8.99;
-  result = format`${quantity} items cost $${(quantity * priceEach).toFixed(
-    2
-  )}.`;
-
-console.log(result); // 9 items cost $80.91.
+let {firstname: manzi, secondnameme: khalidi} = a;
+console.log(person);
