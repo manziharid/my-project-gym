e6b2f69a958a26c2434b5273e7e06af5b10969fc
Haridi Manzi <manziharidi@gmail.com>
2025-07-13 13:08:18 +0200

    my fifth commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,7 +1,3 @@
-let post = {
-    title: 'JavaScript Template Literals',
-    excerpt: 'Introduction to JavaScript template literals in ES6',
-    body: 'Content of the post will be here...',
-    tags: ['es6', 'template literals', 'javascript']
-};
-console.log(post);
+let a = 'khalid';
+let b = 'manzi';
+console.log(`my name's ${a} ${b}`);
