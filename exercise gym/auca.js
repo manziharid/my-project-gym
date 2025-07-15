8496b24e47d76b1c60e69759fb5ae276858f3bdc
Haridi Manzi <manziharidi@gmail.com>
2025-07-15 13:01:06 +0200

    my 8'th commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,11 +1,5 @@
-let [
-    firstName,
-    lastName,
-    [
-        color1,
-        color2,
-        color3
-    ]
-] = getProfile();
-
-console.log(color1, color2, color3); // Red Green Blue
+let person = {
+    firstName: 'John',
+    lastName: 'Doe'
+};
+console.log(person);
