bdfbd05ab17324f9c892177b6e6e83d4bb715e87
Haridi Manzi <manziharidi@gmail.com>
2025-07-15 12:27:48 +0200

    my fourth commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,4 +1,8 @@
-let a, b;
-[a, b] = [10, 20];
+function getItems() {
+  return [10, 20];
+}
 
-console.log({ a, b });
+let items = getItems();
+let thirdItem = items[2] != undefined ? items[2] : 0;
+
+console.log({ thirdItem }); // 0
