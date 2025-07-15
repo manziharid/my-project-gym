2946bdf3ffffcf3104b1ab58aabad65ac189392c
Haridi Manzi <manziharidi@gmail.com>
2025-07-15 12:18:35 +0200

    my first commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,10 +1,7 @@
 function getScores() {
   return [70, 80, 90];
 }
-let scores = getScores();
 
-let x = scores[0],
-  y = scores[1],
-  z = scores[2];
+let [x, y, z] = getScores();
 
 console.log({ x, y, z });
