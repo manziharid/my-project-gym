d2ca010f1593686a15786ba5f098241c95990951
Haridi Manzi <manziharidi@gmail.com>
2025-07-15 19:29:49 +0200

    my 14'th commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,10 +1,8 @@
-function Person(name) {
-    this.name = name;
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
 }
 
-Person.prototype.getName = function () {
-    return this.name;
-};
-
-var john = new Person("John Doe");
-console.log(john.getName());
+let person = new Person("John");
+console.log(person.name); // John
