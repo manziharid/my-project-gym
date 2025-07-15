6223e6fa58e0cf767975355dd5e67edc2845d3f7
Haridi Manzi <manziharidi@gmail.com>
2025-07-15 13:10:45 +0200

    my 11'th commit

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,10 +1,11 @@
 let person = {
     firstName: 'John',
     lastName: 'Doe',
+    middleName: 'C.',
     currentAge: 28
 };
 
-let { firstName, lastName, middleName = '', currentAge: age = 18 } = person;
+let { firstName,  lastName, middleName = '', currentAge: age = 18 } = person;
 
-console.log(middleName); // ''
+console.log(middleName); // 'C.'
 console.log(age); // 28
