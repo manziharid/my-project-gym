6e33e5c040d2d3f44c963145a67de8a2ad5506af
Haridi Manzi <manziharidi@gmail.com>
2025-07-16 21:16:43 +0200

    ghdcb

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,15 +1,8 @@
-let meeting = {
-    attendees: [],
-    add(attendee) {
-        console.log(`${attendee} joined the meeting.`);
-        this.attendees.push(attendee);
-        return this;
-    },
-    get latest() {
-        let count = this.attendees.length;
-        return count == 0 ? undefined : this.attendees[count - 1];
+new class {
+    constructor(name) {
+        this.name = name;
     }
-};
-
-meeting.add('John').add('Jane').add('Peter');
-console.log(`The latest attendee is ${meeting.latest}.`);
+    start() {
+        console.log(`Starting the ${this.name}...`);
+    }
+}
