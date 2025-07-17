9efe1cc098dec3517ab71ed0dcef4532478208a3
Haridi Manzi <manziharidi@gmail.com>
2025-07-17 20:22:29 +0200

    bbhejdn

--- exercise gym/auca.js
+++ exercise gym/auca.js
@@ -1,16 +1,6 @@
-class Item {
-  constructor(name, quantity) {
-    this.name = name;
-    this.quantity = quantity;
-    this.constructor.count++;
-  }
-  static count = 0;
-  static getCount() {
-    return Item.count;
-  }
-}
+// Item class ...
 
-let pen = new Item('Pen', 5);
-let notebook = new Item('notebook', 10);
+let pen = new Item("Pen", 5);
+let notebook = new Item("notebook", 10);
 
 console.log(Item.getCount()); // 2
