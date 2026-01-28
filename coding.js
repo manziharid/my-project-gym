0b3bcc8beacf97056a2b40bc5e2c9822ff76871e
manziharid <manziharidi@gmail.com>
2026-01-28 18:32:52 +0100

    hjed

--- coding.js
+++ coding.js
@@ -1,5 +1,10 @@
+let names = [];
 
-    for (let i = 1; i <= 5; i++) {
-        prompt("Enter name " + i + ":");
-    }
-    console.log (i);
+for (let i = 1; i <= 5; i++) {
+    let name = prompt("Enter name " + i + ":");
+    names.push(name);
+}
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
