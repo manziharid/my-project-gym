4a63fb589d2ce5f4b7e9098d5ff30b5a9da45c4e
manziharid <manziharidi@gmail.com>
2026-01-28 18:35:20 +0100

    mdnhj

--- coding.js
+++ coding.js
@@ -1,5 +1,3 @@
-let num1 = Number(prompt("Enter first number:"));
-let num2 = Number(prompt("Enter second number:"));
-
-let sum = num1 + num2;
-alert("The sum is: " + sum);
+let a = 10;
+let b = 20;
+console.log(a * b);
